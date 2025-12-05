18ac630a4f4468e378ed53876230eea0ee513474
Alexis Kiehn <alexiskiehn@icloud.com>
2025-12-05 11:33:32 +0100

    feat: google cloud data load/upload

---
+++ etrace/upload_data.py
@@ -0,0 +1,22 @@
+from google.cloud.storage import Blob, Client
+
+client = Client()
+
+
+def upload_to_gcs(bucket_name: str, destination_blob_name: str, source_file_name: str):
+    """Uploads a file to Google Cloud Storage."""
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        f"File {source_file_name} uploaded to {destination_blob_name} in bucket {bucket_name}."
+    )
+
+
+if __name__ == "__main__":
+    # Example usage
+    upload_to_gcs(
+        "etrace-data", "data/area.ipynb", "/Users/ak/code/AlexisKiehn/etrace/area.ipynb"
+    )
